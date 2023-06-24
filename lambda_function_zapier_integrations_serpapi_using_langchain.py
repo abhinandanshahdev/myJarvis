cb5910fd64c93ba2c7306c9c6371f6833037d0b9
Abhinandan Shah <46406497+abhinandanshahdev@users.noreply.github.com>
2023-06-24 22:31:45 +0530

    Update lambda_function_zapier_integrations_serpapi_using_langchain.py

--- lambda_function_zapier_integrations_serpapi_using_langchain.py
+++ lambda_function_zapier_integrations_serpapi_using_langchain.py
@@ -17,18 +17,18 @@ def lambda_handler(event, context):
 
     llm=ChatOpenAI(
         openai_api_key=os.getenv("OPENAI_API_KEY"),
-        temperature=0, model_name="gpt-4", max_tokens=100
+        temperature=0.9, model_name="gpt-4", max_tokens=500
     )
+    
+    serpapi_api_key = os.environ['SERP_API_KEY']
     
     zapier = ZapierNLAWrapper()
     
     toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
 
-
-    serpapi_api_key = os.environ['SERP_API_KEY']
-
+    tools = load_tools(["serpapi"], llm=llm, serpapi_api_key=serpapi_api_key)
    
-    agent = initialize_agent(toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    agent = initialize_agent(toolkit.get_tools()+tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
     # Get input from API Gateway and parse x-www-form-urlencoded data
     prompt = event['body']
     
